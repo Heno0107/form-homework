af430cd62a10fd353cd1e58d81e206e9769c48eb
Heno0107 <henripetrosyan47@gmail.com>
2025-04-29 22:54:19 +0300

    start

---
+++ rr/src/components/form/form.jsx
@@ -0,0 +1,6 @@
+import style from './form.module.css'
+import { User } from '../user/user'
+
+export function Form () {
+    
+}
---
+++ rr/src/components/user/user.jsx
@@ -0,0 +1,6 @@
+import style from './user.module.css'
+import { Form } from '../form/form'
+
+export function User ({userName, userLname, userEmail, userPass}) {
+    
+}
