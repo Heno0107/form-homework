ed638a1ddaf20fc2bdf71d5c0cd97ac8dd5d9712
Heno0107 <henripetrosyan47@gmail.com>
2025-04-30 15:14:41 +0300

    finish

---
+++ rr/src/components/error/error.jsx
@@ -0,0 +1,5 @@
+import style from './error.module.css'
+
+export function Error () {
+    <h2>Please, fill the fields</h2>
+}
--- rr/src/components/form/form.jsx
+++ rr/src/components/form/form.jsx
@@ -1,5 +1,6 @@
 import style from './form.module.css'
 import { User } from '../user/user'
+import { Error } from '../error/error'
 import { useState } from 'react'
 
 export function Form () {
@@ -8,6 +9,15 @@ export function Form () {
     const [email , setEmail] = useState('')
     const [password , setPassword] = useState('')
     const [repPassword , setRepPassword] = useState('')
+    const [showUser , setShowUser] = useState(true)
+
+    function btnLogic () {
+        if(name && lastName && email && password && password == repPassword){
+            return true
+        }else {
+            return false
+        }
+    }
 
     return(
         <div className={style.form}>
@@ -18,15 +28,21 @@ export function Form () {
 
             <div className={style.inputs}>
                 <form action="">
-                    <input type="text" className={style.user} placeholder='Name' value = {name} onChange={(e) => setName(e.target.value)}/>
-                    <input type="text" className={style.user} placeholder='Last Name' value = {lastName} onChange={(e) => setLastName(e.target.value)}/>
-                    <input type="email" className={style.pass} placeholder='Email' value = {email} onChange={(e) => setEmail(e.target.value)}/>
-                    <input type="password" className={style.pass} placeholder='Password' value = {password} onChange={(e) => setPassword(e.target.value)}/>
-                    <input type="password" className={style.pass} placeholder='Repeat Password' value={repPassword} onChange={(e) => setRepPassword(e.target.value)}/>
+                    <input type="text" className={showUser == 'decline' ? style.UserError : style.user} placeholder='Name' value = {name} onChange={(e) => setName(e.target.value)}/>
+                    <input type="text" className={showUser == 'decline' ? style.UserError : style.user} placeholder='Last Name' value = {lastName} onChange={(e) => setLastName(e.target.value)}/>
+                    <input type="email" className={showUser == 'decline' ? style.PassError : style.pass} placeholder='Email' value = {email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" className={showUser == 'decline' ? style.PassError : style.pass} placeholder='Password' value = {password} onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="password" className={showUser == 'decline' ? style.PassError : style.pass} placeholder='Repeat Password' value={repPassword} onChange={(e) => setRepPassword(e.target.value)}/>
+ 
+                    <button type='button' onClick={() => {name && lastName && email && password && password == repPassword ? setShowUser('accept') : setShowUser('decline')}}>Sign Up</button>
+                    {showUser == 'decline' && <Error />}
+                    {showUser == 'accept' && <User userName={name} userLname={lastName} userEmail={email} userPass={password}/>}
 
-                    <button type='button'>Sign Up</button>
                 </form>
             </div>
+
+            
         </div>
     )
 }
+
